fc889ed851eea32ff1293b7b85aa6652cc0c7eb3
Robin Schneider <ypid23@aol.de>
2014-03-04 20:33:41 +0100

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(name         = 'pyopening_hours',
+      version      = '0.1',
+      description  = 'Python module providing access to the opening_hours.js library which is written in JavaScript.',
+      author       = 'Robin `ypid` Schneider',
+      author_email = 'ypid23@aol.de',
+      url          = 'https://github.com/ypid/pyopening_hours',
+      packages     = ['osm_time', 'osm_time.OpeningHours'],
+      package_dir  = {'osm_time': 'osm_time/'},
+      package_data = {'osm_time': ['node_modules/']},
+      license      = 'BSD',
+     )
